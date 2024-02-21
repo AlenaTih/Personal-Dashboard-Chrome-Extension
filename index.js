e061715c3db512bec00725fe0753f70d1c347a2e
Alena <alyonatihomirova9@gmail.com>
2024-02-21 17:35:20 +0300

    Update

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-const data = await response.json()
-document.body.style.backgroundImage = `url(${data.urls.regular})`
-document.getElementById("author").textContent = `By: ${data.user.name}`
-
-//     .catch(err => {
-//         // Use a default background image/author
-//         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-// )`
-// 		document.getElementById("author").textContent = `By: Dodi Achmad`
-//     })
-
-const cryptoResponse = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-if (!cryptoResponse.ok) {
-    throw Error("Something went wrong")
+try {
+    const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await response.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (error) {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+    )`
+	document.getElementById("author").textContent = `By: Dodi Achmad`
 }
-// return cryptoResponse.json()
-const cryptoData = cryptoResponse.json()
-document.getElementById("crypto-top").innerHTML = `
-    <img src=${cryptoData.image.small} />
-    <span>${cryptoData.name}</span>
-`
-document.getElementById("crypto").innerHTML += `
-    <p>🎯: $${cryptoData.market_data.current_price.usd}</p>
-    <p>👆: $${cryptoData.market_data.high_24h.usd}</p>
-    <p>👇: $${cryptoData.market_data.low_24h.usd}</p>
-`
 
-    // .catch(err => console.error(err))
+try {
+    const cryptoResponse = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!cryptoResponse.ok) {
+        throw Error("Something went wrong")
+    }
+    const cryptoData = await cryptoResponse.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${cryptoData.image.small} />
+        <span>${cryptoData.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+        <p>🎯: $${cryptoData.market_data.current_price.usd}</p>
+        <p>👆: $${cryptoData.market_data.high_24h.usd}</p>
+        <p>👇: $${cryptoData.market_data.low_24h.usd}</p>
+    `
+} catch (error) {
+    console.error(error)
+}
 
 function getCurrentTime() {
     const date = new Date()
@@ -35,40 +36,38 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
-// navigator.geolocation.getCurrentPosition(position => {
-//     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw Error("Weather data not available")
-//             }
-//             return response.json()
-//         })
-//         .then(data => {
-//             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-//             document.getElementById("weather").innerHTML = `
-//                 <img src=${iconUrl} />
-//                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-//                 <p class="weather-city">${data.name}</p>
-//             `
-//         })
-//         .catch(err => console.error(err))
+// navigator.geolocation.getCurrentPosition(async position => {
+//     try {
+//         const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+//         if (!res.ok) {
+//             throw Error("Weather data not available")
+//         }
+//         const locationData = await res.json()
+//         const iconUrl = `http://openweathermap.org/img/wn/${locationData.weather[0].icon}@2x.png`
+//         document.getElementById("weather").innerHTML = `
+//             <img src=${iconUrl} />
+//             <p class="weather-temp">${Math.round(locationData.main.temp)}º</p>
+//             <p class="weather-city">${locationData.name}</p>
+//         `
+//     } catch (error) {
+//         console.error(error)
+//     }
 // })
 
 // I don't want to allow Google to always track my location, so I hard coded my coordinates
 
-fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=59.937500&lon=30.308611&units=metric`)
-    .then(response => {
-        if (!response.ok) {
-            throw Error("Weather data not available")
-        }
-        return response.json()
-    })
-    .then(data => {
-        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        document.getElementById("weather").innerHTML = `
-            <img src=${iconUrl} />
-            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-            <p class="weather-city">${data.name}</p>
-        `
-    })
-    .catch(err => console.error(err))
+try {
+    const weatherResponse = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=59.937500&lon=30.308611&units=metric`)
+    if (!weatherResponse.ok) {
+        throw Error("Weather data not available")
+    }
+    const weatherData = await weatherResponse.json()
+    const iconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+    document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} />
+        <p class="weather-temp">${Math.round(weatherData.main.temp)}º</p>
+        <p class="weather-city">${weatherData.name}</p>
+    `
+} catch (error) {
+    console.error(error)
+}
